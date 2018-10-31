588a95b49949c0bb4c6d218da241868e6cf7c241
Dan Hunter <dan.hunter55@yahoo.com>
2018-10-31 16:17:00 +0000

    started workshop 10

---
+++ models/index.js
@@ -0,0 +1,4 @@
+var Person = require('./Person')
+
+var dan = new Person('Dan Hunter', 30)
+dan.displayInfo()
--- models/person.js
+++ models/person.js
@@ -1,1 +1,17 @@
 // Your Person class goes here!
+class Person{
+    condtructor(fullName,age){
+        this.fullName = fullName
+        this.age = age 
+
+    }
+    displayInfo(){
+        var lines = [
+            this.fullname,
+            this.age
+        ] 
+        return lines.join('\n')
+    }
+}
+
+module.exports = Person
